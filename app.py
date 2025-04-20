6863fb67557c10c99502e38ab33edad15784c284
Tu Nombre <tuemail@ejemplo.com>
2025-04-20 15:57:18 -0500

    cambios

--- app.py
+++ app.py
@@ -95,8 +95,8 @@ def categoria(nombre):
 
 @app.route('/agregar_producto', methods=['GET', 'POST'])
 def agregar_producto():
-    password = request.args.get('ADMIN_PASSWORD')
-        if password != os.getenv('pollito'):
+    password = request.args.get('password')
+        if password != os.getenv('ADMIN_PASSWORD'):
         return "Acceso denegado", 403
     if request.method == 'POST':
         name = request.form['name']
