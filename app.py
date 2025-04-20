3fec1fc76aeea29f9abcb4a3b4a7893e320abcca
Tu Nombre <tuemail@ejemplo.com>
2025-04-19 19:45:52 -0500

    se crea render.yaml se crea .env y se modifica requirements.tx

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+load_dotenv()
 import os
 
 # Configuración de la aplicación
