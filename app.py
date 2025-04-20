6b794f1e3eafbc03d1fe81450d75f9888016dedc
Tu Nombre <tuemail@ejemplo.com>
2025-04-19 20:29:23 -0500

    actualizacion app.py

--- app.py
+++ app.py
@@ -2,18 +2,17 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
+load_dotenv()
 import os
 
-load_dotenv()
-
 # Configuración de la aplicación
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///productos.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+env_app = Flask(__name__)
+env_app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///productos.db')
+env_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Inicialización de extensiones
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = SQLAlchemy(env_app)
+migrate = Migrate(env_app, db)
 
 # Modelo de producto
 class Product(db.Model):
@@ -22,72 +21,71 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     type = db.Column(db.String(50), nullable=False)
 
-# Ruta principal: lista todos los productos
-@app.route('/')
+# Crear las tablas automáticamente al iniciar (incluso en Render)
+with env_app.app_context():
+    db.create_all()
+
+# Rutas de tu sitio
+@env_app.route('/')
 def index():
     productos = Product.query.all()
     return render_template('index.html', products=productos)
 
-# Rutas de categorías de productos
-@app.route('/bases')
+@env_app.route('/bases')
 def bases():
     productos = Product.query.filter_by(type='base').all()
     return render_template('bases.html', products=productos)
 
-@app.route('/labiales')
+@env_app.route('/labiales')
 def labiales():
     productos = Product.query.filter_by(type='labial').all()
     return render_template('labiales.html', products=productos)
 
-@app.route('/brochas')
+@env_app.route('/brochas')
 def brochas():
     productos = Product.query.filter_by(type='brocha').all()
     return render_template('brochas.html', products=productos)
 
-@app.route('/pestaninas')
+@env_app.route('/pestaninas')
 def pestaninas():
     productos = Product.query.filter_by(type='pestanina').all()
     return render_template('pestaninas.html', products=productos)
 
-@app.route('/rubores')
+@env_app.route('/rubores')
 def rubores():
     productos = Product.query.filter_by(type='rubor').all()
     return render_template('rubores.html', products=productos)
 
-@app.route('/cuidado_capilar')
+@env_app.route('/cuidado_capilar')
 def cuidado_capilar():
     productos = Product.query.filter_by(type='cuidado_capilar').all()
     return render_template('cuidado_capilar.html', products=productos)
 
-@app.route('/correctores')
+@env_app.route('/correctores')
 def correctores():
     productos = Product.query.filter_by(type='corrector').all()
     return render_template('correctores.html', products=productos)
 
-# Rutas de páginas estáticas
-@app.route('/popular')
+@env_app.route('/popular')
 def popular():
     return render_template('popular.html')
 
-@app.route('/ofertas')
+@env_app.route('/ofertas')
 def ofertas():
     return render_template('ofertas.html')
 
-@app.route('/envios')
+@env_app.route('/envios')
 def envios():
     return render_template('envios.html')
 
-@app.route('/contacto')
+@env_app.route('/contacto')
 def contacto():
     return render_template('contacto.html')
 
-@app.route('/categoria/<nombre>')
+@env_app.route('/categoria/<nombre>')
 def categoria(nombre):
     productos = Product.query.filter_by(type=nombre).all()
     return render_template('categoria.html', categoria=nombre, products=productos)
 
 if __name__ == '__main__':
-    # Crear tablas si no existen
-    with app.app_context():
-        db.create_all()
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    env_app.run(host='127.0.0.1', port=5000, debug=True)
